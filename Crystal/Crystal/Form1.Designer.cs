adaa3ca40864ae1e0d0a4d60758542223447ddba
sanjay <sanjai1990kumar@gmail.com>
2017-08-29 22:56:39 +0530

    speech

--- Crystal/Crystal/Form1.Designer.cs
+++ Crystal/Crystal/Form1.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(213, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Say Hello!!!";
+            this.label1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(558, 378);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Crystal/Crystal/Form1.cs
+++ Crystal/Crystal/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Collections;
 
 namespace Crystal
 {
     public partial class Form1 : Form
     {
+        SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices ch = new Choices();
+            ch.Add("Hello");
+            GrammarBuilder gbuilder = new GrammarBuilder();
+            gbuilder.Append(ch);
+            Grammar grammer = new Grammar(gbuilder);
+
+            recognizer.LoadGrammarAsync(grammer);
+
+            recognizer.SetInputToDefaultAudioDevice();
+
+            recognizer.SpeechRecognized += Recognized;
+        }
+
+        private void Recognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text == "Hello")
+            {
+                ArrayList list = new ArrayList();
+                using (SpeechSynthesizer speech = new SpeechSynthesizer())
+                {
+
+                    foreach (InstalledVoice voice in speech.GetInstalledVoices())
+                    {
+                        VoiceInfo info = voice.VoiceInfo;
+                        list.Add(info.Name);
+                    }
+
+                    speech.SetOutputToDefaultAudioDevice();
+                    speech.Speak("I am Sorry Rattu. Please Dont be angry on me. I was simply fighting with you. And you know what, my rattu is very very good girl. please dont say that she is bad.");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            button1.Enabled = false;
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
         }
     }
 }
